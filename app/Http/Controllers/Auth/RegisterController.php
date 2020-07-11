2188ff328a74ae15f0d1244071cdd1bbcf02a944
vagrant <vagrant@homestead>
2020-07-11 08:18:17 +0000

    邮箱认证

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,7 +42,7 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
+     * Get a validator for an iFFncoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
